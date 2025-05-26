928ce57cb796c57170c4d9929aadc476b87ec97e
Admin <admin@Admins-MacBook-Pro.local>
2025-05-26 16:33:07 +0530

    First commit

---
+++ anime.js
@@ -0,0 +1,5 @@
+const animeList = [
+  { title: "Naruto", image: "assets/cover-images/naruto.jpg" , video: "videos/naruto.mp4"},
+  { title: "Tokyo ghoul", image: "assets/cover-images/kaneki.jpg",video: "videos/Tokyo ghoul.mp4" },
+  { title: "One Piece", image: "assets/cover-images/luffy.jpg", video: "videos/One Piece.mp4" }
+];
