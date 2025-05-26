ab775baf61cc0a3c5422b78c3bcf1f1230e842aa
Admin <admin@Admins-MacBook-Pro.local>
2025-05-26 19:26:33 +0530

    ADDED GOOGLE DRIVE IFRAME TEST

--- anime.js
+++ anime.js
@@ -1,5 +1,5 @@
 const animeList = [
   { title: "Naruto", image: "assets/cover-images/naruto.jpg" , video: "videos/naruto.mp4"},
   { title: "Tokyo ghoul", image: "assets/cover-images/kaneki.jpg",video: "videos/Tokyo ghoul.mp4" },
-  { title: "One Piece", image: "assets/cover-images/luffy.jpg", video: "videos/One Piece.mp4" }
+  { title: "One Piece", image: "assets/cover-images/luffy.jpg", video: "https://drive.google.com/uc?export=download&id=1FVaF4RpLbaQNCDycCFIoKPDRNZuPLUWs" }
 ];
