6ec329b36c4d1aa37a7332c3eaa22bb8a3f8d11a
Admin <admin@Admins-MacBook-Pro.local>
2025-06-03 20:01:02 +0530

    added multiple eps list

--- anime.js
+++ anime.js
@@ -1,5 +1,5 @@
 const animeList = [
-  { title: "Naruto", image: "assets/cover-images/naruto.jpg" , video: "videos/naruto.mp4"},
-  { title: "Tokyo ghoul", image: "assets/cover-images/kaneki.jpg",video: "videos/Tokyo ghoul.mp4" },
-  { title: "One Piece", image: "assets/cover-images/luffy.jpg", video: "videos/One Piece.mp4" }
+  { title: "Naruto", image: "assets/cover-images/naruto.jpg" , episodes : ["videos/naruto.mp4","videos/Tokyo ghoul.mp4","videos/naruto.mp4"]},
+  { title: "Tokyo ghoul", image: "assets/cover-images/kaneki.jpg", episodes : "videos/Tokyo ghoul.mp4" },
+  { title: "One Piece", image: "assets/cover-images/luffy.jpg", episodde : "videos/One Piece.mp4" }
 ];
